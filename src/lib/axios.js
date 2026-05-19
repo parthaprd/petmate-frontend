613eb4eb748677af6f49811782be6aceffa41ac1
parthaprd <parthapratimd16@gmail.com>
2026-05-20 01:51:33 +0600

    fix: hardcode api baseURL to relative path /api for local API routes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-let baseURL = process.env.NEXT_PUBLIC_API_URL || '/api';
-if (baseURL && !baseURL.startsWith('http://') && !baseURL.startsWith('https://') && !baseURL.startsWith('/')) {
-  baseURL = `https://${baseURL}`;
-}
-
 const api = axios.create({
-  baseURL,
+  baseURL: '/api',
   withCredentials: true,
 });
 
